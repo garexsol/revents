f6227830de76036725dc6303aecd8783ba3e0ccd
Gary McCann <gary@mylanguagechallenge.com>
2021-07-03 09:48:16 +0100

    End of section 3

---
+++ src/app/layouts/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboards';
+import NavBar from '../../features/nav/NavBar';
+
+function App() {
+  const [formOpen, setFormOpen] = useState(false);
+  return (
+    <>
+      <h1>Re-vents</h1>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
